445671043da8e34d2079a9ec6941a649830913fe
DaTho7561 <DaTho7561@7JKN6Y1.Student.UGDSB.ED>
2016-09-22 13:17:39 -0400

    here is some moar code :-)

--- src/gittutorial/GitTutorial.java
+++ src/gittutorial/GitTutorial.java
@@ -17,6 +17,9 @@ public class GitTutorial {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Liverwurst");
+        }
     }
     
 }
